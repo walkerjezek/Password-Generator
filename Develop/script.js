9d2ac1e38e9625e8d419ce63d78d489b55f67697
walkerj <wpjezek@gmail.com>
2022-03-09 17:28:33 -0700

    Added the logic for the prompts. Next need to figure out how to randomize the characters in the password and display it

--- Develop/script.js
+++ Develop/script.js
@@ -2,17 +2,51 @@
 var generateBtn = document.querySelector("#generate");
 
 //variables
-var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var lower = "abcdefghijklmnopqrstuvwxyz";
-var special = "!@#$%^&*()[]{}<>?/\|+=-_";
-var numbers = "1234567890";
+var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+var lower = "abcdefghijklmnopqrstuvwxyz".split("");
+var special = "!@#$%^&*()[]{}<>?/\|+=-_".split("");
+var numbers = "1234567890".split("");
 var length = "";
-var finalPassword
+var finalPassword = [];
+var password = [];
 
-function password () {
-  length = window.prompt("Please enter a password length between 8 and 128 charachters.")
+function generatePassword () {
+  length = window.prompt("Please enter a password length between 8 and 128 characters.");
+
+  if (length < 8 && length > 128) {
+    window.alert("The password must be between 8 and 128 characters long.");
+    return;
+  }
+//window confirm prompts
+  var upperPrompt = window.confirm("Would you like to include uppercase characters?");
+    if (upperPrompt === true) {
+      password = password.concat(upper);
+    }
+  var lowerPrompt = window.confirm("Would you like to include lowercase characters?");
+    if (lowerPrompt === true) {
+      password = password.concat(lower);
+    }
+  var specialPrompt = window.confirm("Would you like to include special characters?");
+    if (special === true) {
+      password = password.concat(special);
+    }
+  var numbersPrompt = window.confirm("Would you like to include numbers?");
+    if (numbers === true) {
+      password = password.concat(numbers);
+    }
+
+// randomize the characters in the password?
+
 
 }
+
+
+//1. length
+//2. ask upper
+//3. ask lower
+//4. ask special
+//5. ask number
+//6. display final password
 
 
 // Write password to the #password input
