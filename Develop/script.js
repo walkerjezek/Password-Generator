fe86b2326caeb05c2b2703325b60df59af84d555
walkerj <wpjezek@gmail.com>
2022-03-12 08:17:22 -0700

    Fixed everything. Turns out I didn't need to convert to an array. I could just keep it as a string. Simplified everything

--- Develop/script.js
+++ Develop/script.js
@@ -1,57 +1,76 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-//variables
-var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
-var lower = "abcdefghijklmnopqrstuvwxyz".split("");
-var special = "!@#$%^&*()[]{}<>?/.,+=-_".split("");
-var numbers = "1234567890".split("");
-var length = "";
-var finalPassword = [];
-var password = [];
+//global variables
+var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lower = "abcdefghijklmnopqrstuvwxyz";
+var special = "!@#$%^&*()[]{}<>?/.,+=-_";
+var numbers = "1234567890";
 
+
+//function to generate the password
 function generatePassword () {
-  length = window.prompt("Please enter a password length between 8 and 128 characters.");
+  var length = window.prompt("Please enter a password length between 8 and 128 characters.");
 
-  console.log(length);
+  console.log(typeof length == 'number');
+  //local variables
+  var password = "";
+  var characters = "";
+
 
   if (length < 8 || length > 128) {
     window.alert("The password must be between 8 and 128 characters long.");
-    return;
+    return password;
   }
 //window confirm prompts
   var upperPrompt = window.confirm("Would you like to include uppercase characters?");
     if (upperPrompt === true) {
-      password = password.concat(upper);
+      characters += upper;
+      //Add to the list of guarenteed characters that must be included in final password.
+      var index = Math.floor(Math.random() * upper.length);
+      password += upper.charAt(index);
     }
-    console.log(password);
+
   var lowerPrompt = window.confirm("Would you like to include lowercase characters?");
     if (lowerPrompt === true) {
-      password = password.concat(lower);
+      characters += lower;
+      var index = Math.floor(Math.random() * lower.length);
+      password += lower.charAt(index);
     }
-    console.log(password);
+
   var specialPrompt = window.confirm("Would you like to include special characters?");
     if (specialPrompt === true) {
-      password = password.concat(special);
+      characters += special;
+      var index = Math.floor(Math.random() * special.length);
+      password += special.charAt(index);
     }
-    console.log(password);
+
   var numbersPrompt = window.confirm("Would you like to include numbers?");
     if (numbersPrompt === true) {
-      password = password.concat(numbers);
+      characters += numbers;
+      var index = Math.floor(Math.random() * numbers.length);
+      password += numbers.charAt(index);
     }
-    console.log(password);
 
-    //making sure all the characters are included
-    console.log(password.toString());
+  if (password.length === 0) {
+    window.alert("You must choose at least one option.");
+    return password;
+  } 
 
 // randomize the characters in the password?
-    for (i = 0; i < length; i++) {
-      password += Math.floor(Math.random() * length);
-    }
+    for (i = password.length; i < length; i++) {
+      var index = Math.floor(Math.random() * characters.length);
+      password += characters.charAt(index);
+    } 
 
     console.log(password);
-
+    password = password.split('').sort(function(){return 0.5-Math.random()}).join('');
+    return password;
 }
+
+//If selection is true, grab one character from array and store in guarenteedCharacters. Concat to end each time.
+//Then concat the guarenteedCharacters to the randomized characters to ensure you have at least one
+//of each selected character.
 
 
 //1. length
